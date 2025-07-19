c6a3b442a57b7eb68283b7ba56058adc8cef0b9e
klaro115 <klaro115@hotmail.com>
2025-07-19 16:21:24 +0200

    Added main scene, Added GameManager script on root node

---
+++ GameManager.gd
@@ -0,0 +1,15 @@
+extends Node3D
+
+var _stress_level = 0.0;
+
+func increase_stress(amount : float):
+	_stress_level += amount
+	update_user_interface()
+
+func decrease_stress(amount : float):
+	_stress_level -= amount
+	update_user_interface()
+
+func update_user_interface():
+	#TODO
+	return
